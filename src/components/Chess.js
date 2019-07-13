55661164d47b1af803bea661b534cbcd7422e218
Angelo Mahrouchi <angelo.mahrouchi@gmail.com>
2019-07-13 18:44:10 +0200

    Prepare the app

---
+++ src/components/Chess.js
@@ -0,0 +1,18 @@
+import React    from 'react';
+
+/**
+ * Chess app main component
+ */
+class Chess extends React.Component {
+    /**
+     * Renders the component
+     * @return {*}
+     */
+    render() {
+        return (
+            <div>Here comes the chess!</div>
+        );
+    }
+}
+
+export default Chess;
