dd8167be41a42abd5fd3310fc48c5a7150975e77
Angelo Mahrouchi <angelo.mahrouchi@gmail.com>
2019-07-13 20:12:15 +0200

    Display the chess board

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,11 +13,46 @@ class Board extends React.Component {
     render() {
         return (
             <div className="board">
-                <Square isWhite={true}/>
-                <Square isWhite={false}/>
+                {this.buildBoard()}
             </div>
         );
     }
+
+    /**
+     * Builds the board
+     * @return {Array}
+     */
+    buildBoard() {
+        const boardSize = 8;
+
+        let board = [];
+        for (let i = 0; i < boardSize; i++) {
+
+            let line = [];
+            for (let j = 0; j < boardSize; j++) {
+
+                const isWhite = (j % 2 === 0 && i % 2 === 0) || (j % 2 !== 0 && i % 2 !== 0);
+
+                const key = '' + i + j;
+                line.push(
+                    <Square isWhite={isWhite}
+                            key={key}
+                    />
+                );
+            }
+
+            board.push(
+                <div className="line"
+                     key={i}
+                >
+                    {line}
+                </div>
+            );
+        }
+
+        return board;
+    }
+
 }
 
 export default Board;
