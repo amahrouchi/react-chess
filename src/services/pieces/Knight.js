6b19fcdbaba93972d808854dca0148bb53747dde
Angelo Mahrouchi <angelo.mahrouchi@gmail.com>
2019-07-14 20:40:01 +0200

    Validate knight moves

--- src/services/pieces/Knight.js
+++ src/services/pieces/Knight.js
@@ -5,6 +5,58 @@ import AbstractPiece from "./AbstractPiece";
  */
 class Knight extends AbstractPiece {
 
+    /**
+     * Whether the pawn can move from a location to another
+     * @param {object} from {x : x, y : y}
+     * @param {object} to {x : x, y : y}
+     * @return {boolean}
+     */
+    canMove(from, to) {
+        // Check from != to
+        if (!super.canMove(from, to)) {
+            return false;
+        }
+
+        // Check available squares
+        let hasMatched = false;
+        for (let move of this.getAvailableMoves()) {
+            const currSquare = {
+                x : from.x + move[0],
+                y : from.y + move[1]
+            };
+
+            if (currSquare.x === to.x && currSquare.y === to.y ) {
+                hasMatched = true;
+                break;
+            }
+        }
+
+        if (!hasMatched) {
+            return false;
+        }
+
+        // Check if there is a piece on the targeted square
+        const targetPiece = this.chessBoard.getPiece(to.x, to.y);
+
+        return targetPiece === null || targetPiece.getColor() !== this.getColor();
+    }
+
+    /**
+     * Lists the knight's available moves
+     * @return {Array}
+     */
+    getAvailableMoves() {
+        return [
+            [1, 2],
+            [1, -2],
+            [-1, 2],
+            [-1, -2],
+            [2, 1],
+            [2, -1],
+            [-2, 1],
+            [-2, -1],
+        ];
+    }
 }
 
 export default Knight;
